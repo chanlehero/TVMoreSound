181268b35c9c9a41c60fc3e65a2d750a8ed6826d
chenhao <chanlehero@hotmail.com>
2018-02-06 23:20:23 +0800

    all submit

--- app/src/main/java/com/ch/tvmoresound/MainActivity.java
+++ app/src/main/java/com/ch/tvmoresound/MainActivity.java
@@ -1,15 +1,26 @@
 package com.ch.tvmoresound;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import com.ch.tvmoresound.serivce.AudioMediaRouteProviderService;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private Intent audioServiceIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,21 +28,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        audioServiceIntent=new Intent(this,AudioMediaRouteProviderService.class);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        Switch toggleButton=(Switch)menu.findItem(R.id.app_action_service_switch).getActionView().findViewById(R.id.toggle_audio_service_button);
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                Log.i(TAG,"start ................isChecked:"+isChecked);
+                if(isChecked){
+                    startService(audioServiceIntent);
+                    Log.i(TAG,"started audio service!");
+                    Toast.makeText(MainActivity.this,"已开启音频服务",Toast.LENGTH_SHORT).show();
+                }else {
+                    stopService(audioServiceIntent);
+                    Log.i(TAG,"stopped audio service!");
+                    Toast.makeText(MainActivity.this,"已关闭音频服务",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
         return true;
     }
 
--- app/src/main/java/com/ch/tvmoresound/MainActivityFragment.java
+++ app/src/main/java/com/ch/tvmoresound/MainActivityFragment.java
@@ -1,15 +1,31 @@
 package com.ch.tvmoresound;
 
+import android.media.MediaRouter;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends ListFragment {
+
+    private static final String TAG = "MainActivityFragment";
+
+    private boolean isScaning = false;
+
+    private List<AudioMediaDeviceItem> audioMediaDeviceItemList = new ArrayList<AudioMediaDeviceItem>();
+
+    private MediaRouter mr;
 
     public MainActivityFragment() {
     }
@@ -19,4 +35,75 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Button scanButton = (Button)this.getActivity().findViewById(R.id.button_scan_devices);
+        if(scanButton!=null) {
+            Log.i(TAG, "button is not null");
+            scanButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Button button = (Button) view;
+                    Log.i(TAG, "start to scan audio devices");
+                    if (isScaning) {
+                        isScaning = false;
+                        button.setText(R.string.action_scan_audio_devices_button_label);
+
+                    } else {
+                        isScaning = true;
+                        button.setText(R.string.action_scan_audio_devices_button_scanning_label);
+                        mr.addCallback(MediaRouter.ROUTE_TYPE_LIVE_AUDIO, new MediaRouter.Callback() {
+                            @Override
+                            public void onRouteSelected(MediaRouter mediaRouter, int i, MediaRouter.RouteInfo routeInfo) {
+                                Log.i(TAG, "onRouteSelected");
+                            }
+
+                            @Override
+                            public void onRouteUnselected(MediaRouter mediaRouter, int i, MediaRouter.RouteInfo routeInfo) {
+                                Log.i(TAG, "onRouteSelected");
+                            }
+
+                            @Override
+                            public void onRouteAdded(MediaRouter mediaRouter, MediaRouter.RouteInfo routeInfo) {
+                                Log.i(TAG, "onRouteSelected");
+                            }
+
+                            @Override
+                            public void onRouteRemoved(MediaRouter mediaRouter, MediaRouter.RouteInfo routeInfo) {
+                                Log.i(TAG, "onRouteSelected");
+                            }
+
+                            @Override
+                            public void onRouteChanged(MediaRouter mediaRouter, MediaRouter.RouteInfo routeInfo) {
+                                Log.i(TAG, "onRouteSelected");
+                            }
+
+                            @Override
+                            public void onRouteGrouped(MediaRouter mediaRouter, MediaRouter.RouteInfo routeInfo, MediaRouter.RouteGroup routeGroup, int i) {
+                                Log.i(TAG, "onRouteSelected");
+                            }
+
+                            @Override
+                            public void onRouteUngrouped(MediaRouter mediaRouter, MediaRouter.RouteInfo routeInfo, MediaRouter.RouteGroup routeGroup) {
+                                Log.i(TAG, "onRouteSelected");
+                            }
+
+                            @Override
+                            public void onRouteVolumeChanged(MediaRouter mediaRouter, MediaRouter.RouteInfo routeInfo) {
+                                Log.i(TAG, "onRouteSelected");
+                            }
+                        });
+                    }
+
+                }
+            });
+        }
+
+    }
+
+
+
+
 }
